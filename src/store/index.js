f8dabb6569dd40cb667d0cf5de58e0f9536197b5
Antony 'Dimrok' Méchin <antony.mechin@gmail.com>
2019-12-27 16:25:26 +0100

    Do not use persistant storage.
    Signed-off-by: Antony 'Dimrok' Méchin <antony.mechin@gmail.com>

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,12 @@
-/* global window */
 import seedrandom from 'seedrandom';
 import assert from 'assert';
 import Vue from 'vue';
 import Vuex from 'vuex';
-import VuexPersist from 'vuex-persist';
 import { pick } from '../modules/wordPicker';
 
 Vue.use(Vuex);
 
-const vuexPersist = new VuexPersist({
-  key: 'codenames',
-  storage: window.localStorage
-});
-
 const store = new Vuex.Store({
-  plugins: [vuexPersist.plugin],
   state: {
     _initialSeed: 0,
     _seed: 0,
